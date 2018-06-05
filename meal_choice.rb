880c2e489aa423f3dc403bf06d151c5ee170f5d2
sirikops <sirikops@hotmail.com>
2018-06-05 17:45:57 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,15 +1,15 @@
 # Here's an example of a 'snacks' method that returns the meal choice passed in to it and defaults to "cheetos" if nothing is passed in.
-def breakfast(food)
+def breakfast(food = "eggs")
   puts "Morning is the right time for #{food}!" 
 end
 breakfast("pancakes")
 
-def lunch(lunch)
+def lunch(lunch = "salad")
   puts "Lunch is the best time for #{lunch}!"
 end
 lunch("sandwiches")
 
-def dinner(dinner)
+def dinner(dinner = "cassarole")
   puts "Dinner is the best time for #{dinner}!"
 end
 dinner("pasta")
